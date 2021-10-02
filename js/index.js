bd80dc1e43d92a9be9aa86401a95debbfccd2956
kvinkel <43136038+kvinkel@users.noreply.github.com>
2021-10-02 15:55:04 +0200

    Update fetch calls to prevent button spam and update github contributions list to use span for data

--- js/index.js
+++ js/index.js
@@ -36,7 +36,7 @@ function writeIntro() {
   }
 }
 
-let pollTimeout;
+let isPollTimeOutCleared = true;
 
 function pollSensorData() {
   fetch('https://allegiant-cichlid-1289.dataplicity.io/sensors.php')
@@ -54,6 +54,7 @@ function pollSensorData() {
     })
     .then(() => {
       pollTimeout = setTimeout(pollSensorData, 2000);
+      isPollTimeOutCleared = false;
     })
     .catch(error => {
       document.getElementById('raspApi').style.display = 'none';
@@ -61,7 +62,12 @@ function pollSensorData() {
     });
 }
 
+let lastClickedContributions = 0;
+
 function updateGitHubContributionAmount() {
+  if (Date.now() - lastClickedContributions < 10000) return;
+  lastClickedContributions = Date.now();
+
   fetch('https://allegiant-cichlid-1289.dataplicity.io/contributions.php')
     .then(response => {
       if (!response.ok) {
@@ -70,9 +76,8 @@ function updateGitHubContributionAmount() {
       return response.json();
     })
     .then(data => {
-      let contributionsLi = document.getElementById('contributions');
-      contributionsLi.innerHTML = data['contributions'];
-      contributionsLi.style.display = null;
+      document.getElementById('contributionAmount').innerHTML = data['contributions'];
+      document.getElementById('contributions').style.display = null;
     })
     .catch(error => {
       document.getElementById('contributions').style.display = 'none';
@@ -89,38 +94,49 @@ function hideElements(className) {
   for (let i = 0; i < content.length; i++) {
     content[i].style.display = 'none';
   }
-  clearTimeout(pollTimeout);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+function clearPollTimeout() {
+  clearTimeout(pollTimeout);
+  isPollTimeOutCleared = true;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
 
   writeIntro();
-  document.getElementById('homeButton').addEventListener('click', function() {
+  document.getElementById('homeButton').addEventListener('click', function () {
     hideElements('content');
     showElement('home');
     showElement('raspApi');
     title.innerHTML = 'Home';
-    pollSensorData();
+    if (isPollTimeOutCleared) {
+      pollSensorData();
+    }
   });
-  document.getElementById('projectsButton').addEventListener('click', function() {
+  document.getElementById('projectsButton').addEventListener('click', function () {
     hideElements('content');
     showElement('projects');
     title.innerHTML = 'Projects';
+    clearPollTimeout();
   });
-  document.getElementById('skillsButton').addEventListener('click', function() {
+  document.getElementById('skillsButton').addEventListener('click', function () {
     hideElements('content');
     showElement('skills');
     title.innerHTML = 'Skills';
+    clearPollTimeout();
   });
-  document.getElementById('aboutButton').addEventListener('click', function() {
+  document.getElementById('aboutButton').addEventListener('click', function () {
     hideElements('content');
     showElement('about');
     title.innerHTML = 'About';
+    updateGitHubContributionAmount();
+    clearPollTimeout();
   });
-  document.getElementById('contactButton').addEventListener('click', function() {
+  document.getElementById('contactButton').addEventListener('click', function () {
     hideElements('content');
     showElement('contact');
     title.innerHTML = 'Contact';
+    clearPollTimeout();
   });
   document.getElementById('contactForm').setAttribute('action', 'https://formspree.io' + '/xwkebjeo');
 
