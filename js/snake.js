ce8cc4f592fc4119fd20d6ac6c333004bff19f5a
kvinkel <43136038+kvinkel@users.noreply.github.com>
2023-07-28 18:53:03 +0200

    Add highscore to snake game

--- js/snake.js
+++ js/snake.js
@@ -3,8 +3,8 @@ export { start, stop }
 // Playing board
 const canvasBlockSize = 25;
 const rowAndColumnLength = 20;
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 
 // Snake and food
 let snakeHead = {x: getRandomCoordinate(), y: getRandomCoordinate()};
@@ -13,8 +13,8 @@ let food = {x: 0, y: 0}
 let snakeBody = [];
 
 // Score
-const scoreElement = document.getElementById("score");
-const highScoreElement = document.getElementById("highScore");
+const scoreElement = document.getElementById('score');
+const highScoreElement = document.getElementById('highScore');
 let highScore = 0;
 
 // Update
@@ -25,7 +25,7 @@ let updateNumberCheck = 1;
 function start() {
     canvas.height = rowAndColumnLength * canvasBlockSize;
     canvas.width = rowAndColumnLength * canvasBlockSize;
-    document.addEventListener("keydown", changeDirection);
+    document.addEventListener('keydown', changeDirection);
     placeFood();
     updateInterval = setInterval(update, 100);
 }
@@ -47,19 +47,19 @@ function reset() {
 }
 
 function update() {
-    scoreElement.innerHTML = snakeBody.length;
+    scoreElement.innerHTML = snakeBody.length.toString();
 
     if (snakeBody.length > highScore) {
         highScore = snakeBody.length;
         highScoreElement.innerHTML = highScore;
     }
 
-    context.fillStyle = "black";
+    context.fillStyle = 'black';
     context.fillRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = "lime";
+    context.fillStyle = 'lime';
     context.fillRect(food.x, food.y, canvasBlockSize, canvasBlockSize);
 
-    if (snakeHead.x == food.x && snakeHead.y == food.y) {
+    if (snakeHead.x === food.x && snakeHead.y === food.y) {
         snakeBody.push([food.x, food.y]);
         placeFood();
     }
@@ -79,7 +79,7 @@ function updateSnake() {
     }
 
     // Fill in snake color
-    context.fillStyle = "white";
+    context.fillStyle = 'white';
     snakeHead.x += delta.x * canvasBlockSize;
     snakeHead.y += delta.y * canvasBlockSize;
     context.fillRect(snakeHead.x, snakeHead.y, canvasBlockSize, canvasBlockSize);
@@ -95,7 +95,7 @@ function checkGameOver() {
     }
 
     for (let i = 0; i < snakeBody.length; i++) {
-        let isSnakeRunningIntoItself = snakeHead.x == snakeBody[i][0] && snakeHead.y == snakeBody[i][1];
+        let isSnakeRunningIntoItself = snakeHead.x === snakeBody[i][0] && snakeHead.y === snakeBody[i][1];
         if (isSnakeRunningIntoItself) {
             reset();
         }
@@ -108,19 +108,19 @@ function changeDirection(event) {
         return;
     }
 
-    if (event.code === "ArrowUp" && delta.y !== 1) {
+    if (event.code === 'ArrowUp' && delta.y !== 1) {
         delta.x = 0;
         delta.y = -1;
     }
-    else if (event.code === "ArrowDown" && delta.y !== -1) {
+    else if (event.code === 'ArrowDown' && delta.y !== -1) {
         delta.x = 0;
         delta.y = 1;
     }
-    else if (event.code === "ArrowLeft" && delta.x !== 1) {
+    else if (event.code === 'ArrowLeft' && delta.x !== 1) {
         delta.x = -1;
         delta.y = 0;
     }
-    else if (event.code === "ArrowRight" && delta.x !== -1) {
+    else if (event.code === 'ArrowRight' && delta.x !== -1) {
         delta.x = 1;
         delta.y = 0;
     }
@@ -146,3 +146,89 @@ function placeFood() {
 function getRandomCoordinate() {
     return Math.floor(Math.random() * rowAndColumnLength) * canvasBlockSize;
 }
+
+// Highscores
+
+function saveScore(username, score) {
+    fetch(h, {
+        method: 'POST',
+        body: JSON.stringify({
+          username: username,
+          score: score,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8'
+        }
+      }).catch(error => {
+        console.error('Error fetching data:', error);
+    });
+}
+
+function populateTop10List() {
+    const listElement = document.getElementById('highScoreList');
+    fetch(t)
+        .then(response => response.json())
+        .then(data => {
+            listElement.innerHTML = '';
+            let count = 0;
+
+            data.forEach(item => {
+                count++;
+                const listItem = document.createElement('li');
+                listItem.textContent = count + '. ' + item.username + ': ' + item.score;
+                listElement.appendChild(listItem);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+// Score modal
+const scoresModal = document.getElementById('scoresModal');
+const openModalScoresBtn = document.getElementById('openModalScoresBtn');
+const closeScoresBtn = document.getElementById('closeScoresModal');
+
+openModalScoresBtn.addEventListener('click', () => {
+    populateTop10List();
+    scoresModal.style.display = 'block';
+});
+
+closeScoresBtn.addEventListener('click', () => {
+    scoresModal.style.display = 'none';
+});
+
+// Save modal
+const saveModal = document.getElementById('saveModal');
+const saveButton = document.getElementById('saveButton');
+const openModalSaveBtn = document.getElementById('openModalSaveBtn');
+const closeSaveBtn = document.getElementById('closeSaveModal');
+
+openModalSaveBtn.addEventListener('click', () => {
+    saveModal.style.display = 'block';
+    saveButton.style.display = 'block';
+    document.getElementById('scoreToSave').innerHTML = highScore;
+});
+
+closeSaveBtn.addEventListener('click', () => {
+    saveModal.style.display = 'none';
+});
+
+function handleSubmit() {
+    const username = document.getElementById('usernameInput').value;
+    saveScore(username, highScore);
+    saveModal.style.display = 'none';
+}
+
+document.getElementById('saveButton').addEventListener('click', handleSubmit);
+
+// Close modal when clicking outside of it
+window.addEventListener('click', (event) => {
+    if (event.target === scoresModal || event.target === saveModal) {
+        scoresModal.style.display = 'none';
+        saveModal.style.display = 'none';
+    }
+});
+
+var _0x1994=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x70\x72\x6F\x66\x69\x63\x69\x65\x6E\x74\x2D\x67\x6F\x73\x68\x61\x77\x6B\x2D\x38\x31\x39\x36\x2E\x64\x61\x74\x61\x70\x6C\x69\x63\x69\x74\x79\x2E\x69\x6F\x2F\x70\x6F\x72\x74\x66\x6F\x6C\x69\x6F\x2F\x68\x69\x67\x68\x73\x63\x6F\x72\x65"];let h=_0x1994[0]
+var _0x3881=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x70\x72\x6F\x66\x69\x63\x69\x65\x6E\x74\x2D\x67\x6F\x73\x68\x61\x77\x6B\x2D\x38\x31\x39\x36\x2E\x64\x61\x74\x61\x70\x6C\x69\x63\x69\x74\x79\x2E\x69\x6F\x2F\x70\x6F\x72\x74\x66\x6F\x6C\x69\x6F\x2F\x74\x6F\x70\x31\x30"];let t=_0x3881[0]
